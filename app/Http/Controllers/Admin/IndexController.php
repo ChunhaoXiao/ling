93f58a05597211b96399cc169a81c4ef8087f375
Your Name <you@example.com>
2019-06-22 18:45:06 +0800

    install redis

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Cache;
+use Illuminate\Support\Facades\Redis;
+
+
 
 class IndexController extends Controller
 {
     public function index()
     {
+    	Redis::set('name', 'xiao');
+    	dump(Redis::get('name'));
     	return view('admin.index.index');
     }
 }
